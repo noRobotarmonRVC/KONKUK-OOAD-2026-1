4682b1d5948dba76fe6a5f7d9aac79e89079d438
bakbakwanwan <parkwanshang@gmail.com>
2026-05-07 00:50:22 +0900

    [KON-19]: correct functions

--- project/src/DriveMotor.cpp
+++ project/src/DriveMotor.cpp
@@ -6,8 +6,8 @@ DriveMotor::DriveMotor(std::shared_ptr<AbstractNetwork> network)
     : m_network(std::move(network)) {}
 
 void DriveMotor::moveForward() {
-    if (m_client) {
-        m_client->sendAsync("MOVE_FORWARD");
+    if (m_network) {
+        m_network->send("MOVE_FORWARD");
     }
 
     // 일단 주석 처리, test 필요 시 유지
@@ -15,32 +15,32 @@ void DriveMotor::moveForward() {
 }
 
 void DriveMotor::moveBackward() {
-    if (m_client) {
-        m_client->sendAsync("MOVE_BACKWARD");
+    if (m_network) {
+        m_network->send("MOVE_BACKWARD");
     }
 
     // std::cout << "[DriveMotor] move backward\n";
 }
 
 void DriveMotor::stop() {
-    if (m_client) {
-        m_client->sendAsync("STOP_MOTOR");
+    if (m_network) {
+        m_network->send("STOP_MOTOR");
     }
 
     // std::cout << "[DriveMotor] stop\n";
 }
 
 void DriveMotor::rotateRight() {
-    if (m_client) {
-        m_client->sendAsync("ROTATE_RIGHT");
+    if (m_network) {
+        m_network->send("ROTATE_RIGHT");
     }
 
     // std::cout << "[DriveMotor] rotate right\n";
 }
 
 void DriveMotor::rotateLeft() {
-    if (m_client) {
-        m_client->sendAsync("ROTATE_LEFT");
+    if (m_network) {
+        m_network->send("ROTATE_LEFT");
     }
 
     // std::cout << "[DriveMotor] rotate left\n";
