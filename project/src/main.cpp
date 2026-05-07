e36b86f3c3543ec063fde7c304c587bddaf3152f
bw-green <bwgreen621@gmail.com>
2026-05-07 11:32:20 +0900

    debug

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,4 +1,11 @@
 #include <memory>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+
+#include <sys/select.h>
+#include <unistd.h>
 
 #include "AbstractNetwork.hpp"
 #include "CleaningController.hpp"
@@ -9,6 +16,18 @@
 #include "SweepingUnit.hpp"
 #include "TCPNetwork.hpp"
 
+bool hasInput() {
+    fd_set set;
+    FD_ZERO(&set);
+    FD_SET(STDIN_FILENO, &set);
+
+    timeval timeout{};
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+
+    return select(STDIN_FILENO + 1, &set, nullptr, nullptr, &timeout) > 0;
+}
+
 int main() {
     auto network = std::make_shared<TCPNetwork>("127.0.0.1", 9000);
 
@@ -16,11 +35,6 @@ int main() {
     auto cleaner = std::make_shared<SweepingUnit>(network);
     auto dustSensor = std::make_shared<DustSensor>(network);
     auto obstacleSensor = std::make_shared<ObstacleSensor>(network);
-
-    // dustSensor->setDustDetected(true);
-
-    // index rule: [0] front, [1] right, [2] back, [3] left
-    // obstacleSensor->setObstacleInfo({1, 0, 0, 0});
 
     auto cleaningController = std::make_unique<CleaningController>(
         motor,
@@ -32,10 +46,39 @@ int main() {
 
     RVC rvc(std::move(cleaningController));
 
-    rvc.powerOn();
-    rvc.startCleaning();
-    rvc.stopCleaning();
-    rvc.powerOff();
+    std::cout << "1: powerOn\n";
+    std::cout << "2: startCleaning\n";
+    std::cout << "3: stopCleaning\n";
+    std::cout << "4: powerOff\n";
+    std::cout << "q: quit\n";
+
+    while (true) {
+        if (hasInput()) {
+            std::string input;
+            std::getline(std::cin, input);
+
+            if (input == "1") {
+                rvc.powerOn();
+                std::cout << "[OK] powerOn\n";
+            } else if (input == "2") {
+                rvc.startCleaning();
+                std::cout << "[OK] startCleaning\n";
+            } else if (input == "3") {
+                rvc.stopCleaning();
+                std::cout << "[OK] stopCleaning\n";
+            } else if (input == "4") {
+                rvc.powerOff();
+                std::cout << "[OK] powerOff\n";
+            } else if (input == "q") {
+                std::cout << "종료합니다.\n";
+                break;
+            } else if (!input.empty()) {
+                std::cout << "잘못된 입력: " << input << '\n';
+            }
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
 
     return 0;
 }
